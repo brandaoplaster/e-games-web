fd2ab1133f3dca0b02c08b6eccf4ba82b26c408a
brandaoplaster <brandaoplaster@gmail.com>
2024-01-08 23:07:39 -0300

    feat: [WIP] in component store front header

--- src/components/shared/header/storefront-header/index.tsx
+++ src/components/shared/header/storefront-header/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
+import style from "@/styles/Header.module.css";
 
 const StoreFrontHeader: React.FC = () => {
   return (
-    <div>
-    </div>
+    <Row className={style.background}>
+      <Col md={8} className="mt-2">
+
+      </Col>
+
+      <Col md={4} className="mt-2 text-center">
+
+      </Col>
+    </Row>
   )
 };
 
