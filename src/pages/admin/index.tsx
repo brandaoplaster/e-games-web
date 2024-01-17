7e53d6924b07fa8b1cf029765b61fa9a96202325
brandaoplaster <brandaoplaster@gmail.com>
2024-01-16 23:39:50 -0300

    feat: Adding HOC withAuthAdmin

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,7 +1,8 @@
+import withAuthAdmin from "@/components/with-auth-admin";
 import React from "react";
 
 const Home: React.FC = () => {
   return <h1>Admin</h1>;
 };
 
-export default Home;
+export default withAuthAdmin(Home);
