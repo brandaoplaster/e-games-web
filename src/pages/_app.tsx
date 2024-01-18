e9f3ce54ed3d1ca120de93bbfee1cec23602cab8
brandaoplaster <brandaoplaster@gmail.com>
2024-01-17 21:34:12 -0300

    feat: Adding react toastity

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,10 @@ import "@/styles/Globals.css";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "@/store";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+toast.configure();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,7 +17,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <PersistGate loading={null} persistor={persistor}>
           <Head>
             <title>E-games</title>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta
+              name="viewport"
+              content="initial-scale=1.0, width=device-width"
+            />
           </Head>
           <Component {...pageProps} />
         </PersistGate>
