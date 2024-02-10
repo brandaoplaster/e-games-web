7b23ff364edc7dd9eaebcf2a7d808276edfb0238
brandaoplaster <brandaoplaster@gmail.com>
2024-02-10 18:54:31 -0300

    feat: Adding component pagination

---
+++ src/components/shared/pagination/index.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import PaginationService from "@/libs/paginationservice";
+import StyleButton from "@/components/shared/style-button";
+import Meta from "@/dtos/meta";
+
+const Pagination: React.FunctionComponent<Meta> = ({
+  page,
+  length,
+  total,
+  total_pages,
+}) => {
+  const [Pagination, setPagination] = useState(["1"]);
+  const router = useRouter();
+
+  useEffect(() => {
+    setPagination(PaginationService.execute(total_pages, page));
+  }, [page, total_pages]);
+
+  const handlePageClick = (page: string): void => {
+    router.push(`${router.pathname}?page=${page}`);
+  };
+
+  const handleNextPageClick = (): void => {
+    if (page < total_pages) {
+      router.push(`${router.pathname}?page=${page + 1}`);
+    }
+  };
+
+  const handlePreviusPageClick = (): void => {
+    if (page > 1) {
+      router.push(`${router.pathname}?page=${page - 1}`)
+    }
+  }
+
+  return (
+    <div className="pagination justify-content-end">
+      <div className="pagination">
+        <StyleButton 
+          action="<"
+          type_button="blue"
+          onClick={handlePreviusPageClick}
+        />
+        {
+          Pagination.map((item, index) =>(
+            item === "..." ? "..." : (
+              <StyleButton 
+                key={index}
+                action={item}
+                type_button="blue"
+                active={page === Number(item)}
+                onClick={() => handlePageClick(item)}
+              />
+            )
+          ))
+        }
+
+        <StyleButton 
+          action=">"
+          type_button="blue"
+          onClick={handleNextPageClick}
+        />
+      </div>
+    </div>
+  )
+};
+
+export default Pagination;
