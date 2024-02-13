6b7f8ffae45c0e071bfe3a1eb82c2d45883919eb
brandaoplaster <brandaoplaster@gmail.com>
2024-02-12 23:44:46 -0300

    feat: Adding category in root reducer

--- src/store/modules/rootReducer.ts
+++ src/store/modules/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import auth from "@/store/modules/auth";
 import search from "@/store/modules/admin/shared/search/reducer";
+import Category from "@/store/modules/admin/category/reducer";
 
 export default combineReducers({
   auth,
-  search
+  search,
+  Category
 });
