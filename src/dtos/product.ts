06170875401b25a8e8a15389a894d722385eb5ed
brandaoplaster <brandaoplaster@gmail.com>
2024-02-17 19:48:17 -0300

    feat: Adding dtos product and system requirement

---
+++ src/dtos/product.ts
@@ -0,0 +1,17 @@
+import Category from "./category";
+import SystemRequirement from "./systemRequirement";
+
+export default interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  status: string;
+  image_url: string;
+  productable: string;
+  mode: string;
+  developer: string;
+  release_date: string;
+  categories: Category[];
+  system_requirements: SystemRequirement;
+}
